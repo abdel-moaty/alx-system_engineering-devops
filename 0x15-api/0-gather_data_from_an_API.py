03ead2879e6df9fa1e6d5718cd3d8983e557c60f
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-10-23 15:56:58 -0700

    Add Python script to display employee's tasks using REST API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''
+Python script to retrieve and display information about an employee's tasks
+using a REST API.
+'''
+
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
+    todo_numer = len(todo_result)
+    todo_complete = len([todo for todo in todo_result
+                         if todo.get("completed")])
+    name = name_result.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, todo_complete, todo_numer))
+    for todo in todo_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
